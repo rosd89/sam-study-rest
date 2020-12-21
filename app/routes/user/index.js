3f104b46ccb89a48c56c14094939f1d4cbba906e
sam <zkdlffpem@naver.com>
2020-12-21 21:16:59 +0900

    유저 추가/삭제/업데이트를 구현합니다

--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -52,7 +52,10 @@ router.get('/', (req, res) => {
 // 유저 상세 조회 API
 router.get('/:id', (req, res) => {
   const {id} = req.params
-  const user = USER_DATA.find(user => user.id === id)
+  const user = USER_DATA.find(user => {
+    if (user.enable === 'disable') return false
+    return user.id === id
+  })
   if (!user) {
     res.status(404).json({})
     return
@@ -68,17 +71,91 @@ router.get('/:id', (req, res) => {
 
 // 유저 생성 API
 router.post('/', (req, res) => {
+  const {id, pw, name} = req.body
+  if (!id) {
+    res.status(400).json({message: '"id"가 입력되지 않았습니다.'})
+    return
+  }
+  if (!pw) {
+    res.status(400).json({message: '"pw"가 입력되지 않았습니다.'})
+    return
+  }
+  if (!name) {
+    res.status(400).json({message: '"name"가 입력되지 않았습니다.'})
+    return
+  }
 
+  const target = USER_DATA.find(user => user.id === id)
+  if (target) {
+    res.status(400).json({message: '지금 요청하신 ID는 이미 존재하는 유저입니다.'})
+    return
+  }
+
+  const now = new Date()
+  const user = new userModel(id, pw, name, 'enable',now, now)
+  USER_DATA.push(user)
+
+  res.json({
+    id: user.id,
+    name: user.name,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt
+  })
 })
 
 // 유저 수정 API
 router.patch('/:id', (req, res) => {
+  const {id} = req.params
+  const {pw, name, enable} = req.body
+  const user = USER_DATA.find(user => {
+    if (user.enable === 'disable') return false
+    return user.id === id
+  })
+  if (!user) {
+    res.status(404).json({})
+    return
+  }
 
+  const updateData = []
+  if (pw && user.pw !== pw) {
+    updateData.push({key: 'pw', data: pw})
+  }
+  if (name && user.name !== name) {
+    updateData.push({key: 'name', data: name})
+  }
+  if (enable && user.enable !== enable) {
+    updateData.push({key: 'enable', data: enable})
+  }
+
+  if (updateData.length === 0) {
+    res.status(400).json({message: '수정된 값이 존재하지 않습니다.'})
+    return
+  } else {
+    updateData.push({key: 'updatedAt', data: new Date()})
+  }
+
+  updateData.forEach(({key, data}) => {
+    user[key] = data
+  })
+
+  res.json({message: '유저데이터 수정에 성공하였습니다.'})
 })
 
 // 유저 삭제 API
 router.delete('/:id', (req, res) => {
+  const {id} = req.params
+  const user = USER_DATA.find(user => {
+    if (user.enable === 'disable') return false
+    return user.id === id
+  })
+  if (!user) {
+    res.status(404).json({})
+    return
+  }
 
+  user.enable = 'disable'
+
+  res.status(204).send()
 })
 
 module.exports = router
