470d5462aff3706c9c661fd619d7df4a912dbb02
sam <zkdlffpem@naver.com>
2021-01-11 21:19:46 +0900

    유저 나머지 API를 DB를 사용하는 방식으로 수정합니다

--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -1,26 +1,11 @@
 const {Router} = require('express')
 
-const userModel = require('./user')
 const service = require('./user.service')
 const {getSalt, getExpiredTime, getHash} = require('../lib/hash')
 
 const router = Router()
 
-let USER_DATA = [
-  new userModel('red', '68add1e4a6a8d4ae0c803a5a88debecf3ffe76437ba14735b1e3b141936941e7', 'aa1b86b930a10cf7a3dbb6f80a1be460e9a3738d062f8108d5040fa60b18a323', '누구개1', 'enable', new Date(), new Date()),
-  new userModel('blue', '68add1e4a6a8d4ae0c803a5a88debecf3ffe76437ba14735b1e3b141936941e7', 'aa1b86b930a10cf7a3dbb6f80a1be460e9a3738d062f8108d5040fa60b18a323', '누구개2', 'enable', new Date(), new Date()),
-  new userModel('yellow', '68add1e4a6a8d4ae0c803a5a88debecf3ffe76437ba14735b1e3b141936941e7', 'aa1b86b930a10cf7a3dbb6f80a1be460e9a3738d062f8108d5040fa60b18a323', '아무개3', 'enable', new Date(), new Date()),
-  new userModel('pink', '68add1e4a6a8d4ae0c803a5a88debecf3ffe76437ba14735b1e3b141936941e7', 'aa1b86b930a10cf7a3dbb6f80a1be460e9a3738d062f8108d5040fa60b18a323', '아무개4', 'enable', new Date(), new Date()),
-  new userModel('black', '68add1e4a6a8d4ae0c803a5a88debecf3ffe76437ba14735b1e3b141936941e7', 'aa1b86b930a10cf7a3dbb6f80a1be460e9a3738d062f8108d5040fa60b18a323', '누구개5', 'disable', new Date(), new Date()),
-  new userModel('gold', '68add1e4a6a8d4ae0c803a5a88debecf3ffe76437ba14735b1e3b141936941e7', 'aa1b86b930a10cf7a3dbb6f80a1be460e9a3738d062f8108d5040fa60b18a323', '아무개6', 'enable', new Date(), new Date())
-]
-
 const connectStatus = {}
-
-const userFindById = id => USER_DATA.find(user => {
-  if (user.enable === 'disable') return false
-  return user.id === id
-})
 
 // 유저 목록을 조회 API
 router.get('/', (req, res) => {
@@ -37,25 +22,13 @@ router.get('/', (req, res) => {
     return
   }
 
-  const users = USER_DATA.filter(user => {
-    if (user.enable === 'disable') return false
-
-    if (name) {
-      if (!user.name.includes(name)) return false
-    }
-
-    return true
-  })
-
-  const offset = (page - 1) * pageSize
-  const items = users.slice(offset, offset + pageSize).map(({pw, enable, ...user}) => user)
-
-  res.json({
-    items,
-    page,
-    pageSize,
-    total: users.length
-  })
+  return service.userFindAll({name, page, pageSize})
+    .then(result => {
+      res.json(result)
+    })
+    .catch(e => {
+      res.status(500).json(e)
+    })
 })
 
 // 유저 생성 API
@@ -77,40 +50,46 @@ router.post('/', async (req, res) => {
     name,
     enabled: 'enable'
   }
-  const [id] = await service.userCreate(user)
+  try {
+    const [id] = await service.userCreate(user)
+    res.json({
+      id,
+      name: user.name,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+    })
+  } catch (e) {
+    res.status(500).json(e)
+  }
 
-  res.json({
-    id,
-    name: user.name,
-    createdAt: user.createdAt,
-    updatedAt: user.updatedAt
-  })
 })
 
 // 유저 로그인 API
 router.post('/signin', (req, res) => {
   // 로그인 API 구현
   const {id, pw} = req.body
-  const user = userFindById(id)
-  if (!user) {
-    res.status(404).json({})
-    return
-  }
+  return service.userFindById(id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({})
+        return
+      }
 
-  if (user.pw !== getHash(pw, user.salt)) {
-    res.status(401).json({message: '입력하신 비밀번호가 올바르지 않습니다.'})
-    return
-  }
+      if (user.pw !== getHash(pw, user.salt)) {
+        res.status(401).json({message: '입력하신 비밀번호가 올바르지 않습니다.'})
+        return
+      }
 
-  const token = getSalt()
-  const expired = getExpiredTime()
-  connectStatus[id] = {
-    token, expired
-  }
+      const token = getSalt()
+      const expired = getExpiredTime()
+      connectStatus[id] = {
+        token, expired
+      }
 
-  res.json({
-    token, expired
-  })
+      res.json({
+        token, expired
+      })
+    })
 })
 
 // 로그인이 되었는지 확인하는 미들웨어 생성
@@ -151,32 +130,35 @@ router.use('/:id', (req, res, next) => {
 // 유저 상세 조회 API
 router.get('/:id', (req, res) => {
   const {id} = req.params
-  const user = USER_DATA.find(user => {
-    if (user.enable === 'disable') return false
-    return user.id === id
-  })
-  if (!user) {
-    res.status(404).json({})
-    return
-  }
+  return service.userFindById(id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({})
+        return
+      }
 
-  res.json({
-    id,
-    name: user.name,
-    createdAt: user.createdAt,
-    updatedAt: user.updatedAt
-  })
+      res.json({
+        id,
+        name: user.name,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt
+      })
+    })
 })
 
 // 유저 수정 API
-router.patch('/:id', (req, res) => {
+router.patch('/:id', async (req, res) => {
   const {id} = req.params
   const {pw, name, enable} = req.body
-  const user = USER_DATA.find(user => {
-    if (user.enable === 'disable') return false
-    return user.id === id
-  })
-  if (!user) {
+
+  let user
+  try {
+    user = await service.userFindById(id)
+    if (!user) {
+      res.status(404).json({})
+      return
+    }
+  } catch (e) {
     res.status(404).json({})
     return
   }
@@ -195,30 +177,36 @@ router.patch('/:id', (req, res) => {
   if (updateData.length === 0) {
     res.status(400).json({message: '수정된 값이 존재하지 않습니다.'})
     return
-  } else {
-    updateData.push({key: 'updatedAt', data: new Date()})
   }
 
-  updateData.forEach(({key, data}) => {
-    user[key] = data
-  })
+  try {
+    await service.userUpdate(user.id, {...updateData, updatedAt: new Date()})
+  } catch (e) {
+    res.status(500).json(e)
+    return
+  }
 
   res.json({message: '유저데이터 수정에 성공하였습니다.'})
 })
 
 // 유저 삭제 API
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const {id} = req.params
-  const user = USER_DATA.find(user => {
-    if (user.enable === 'disable') return false
-    return user.id === id
-  })
-  if (!user) {
-    res.status(404).json({})
+  try {
+    const user = await service.userFindById(id)
+    if (!user) {
+      res.status(404).json({})
+      return
+    }
+
+    await service.userUpdate(user.id, {
+      enable: 'disable',
+      updatedAt: new Date()
+    })
+  } catch (e) {
+    res.status(500).json(e)
     return
   }
-
-  user.enable = 'disable'
 
   res.status(204).send()
 })
--- app/routes/user/user.service.js
+++ app/routes/user/user.service.js
@@ -2,6 +2,25 @@ const db = require('../../../helper/db')
 
 const ACCOUNT = 'account'
 
+const paged = async (q, {key = 'id', fields = '*', page, pageSize, sort=key, order='descend'}) => {
+  const offset = (+page - 1) * +pageSize
+  const cq = q.clone()
+
+  if (sort) {
+    q.orderBy(sort, order === 'descend' ? 'desc' : 'arc')
+  }
+
+  return Promise.all([
+    q.select(fields).limit(pageSize).offset(offset),
+    cq.count({total: key})
+  ]).then(([rows, [{total}]]) => ({
+    page: +page,
+    pageSize: +pageSize,
+    total,
+    items: rows
+  }))
+}
+
 const userFindById = id => {
   if (!id) {
     return Promise.reject('id 값이 없습니다.')
@@ -13,10 +32,30 @@ const userFindById = id => {
     .then(([item]) => item)
 }
 
+const userFindAll = ({name, page = 10, pageSize = 10}) => {
+  const q = db(ACCOUNT)
+    .andWhere('enable', 'enable')
+
+  if (name) {
+    q.andWhere('name', 'like', `%${name}%`)
+  }
+
+  return paged(q, {page, pageSize})
+}
+
 const userCreate = user => {
   return db(ACCOUNT).insert(user)
 }
 
+const userUpdate = (id, user) => {
+  return db(ACCOUNT)
+    .update(user)
+    .andWhere('id', id)
+}
+
 module.exports = {
-  userFindById, userCreate
+  userFindById,
+  userFindAll,
+  userCreate,
+  userUpdate
 }
